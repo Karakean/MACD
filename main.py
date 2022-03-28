8c1d7dabc66665bd3da701413b06b96fc0821a99
Karakean <mipigeno@gmail.com>
2022-03-29 01:06:03 +0200

    improving

--- main.py
+++ main.py
@@ -4,7 +4,28 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-def load_data(path, price_column, date_column):
+def set_price_chart(dates, prices, currency):
+    plt.rcParams['figure.figsize'] = [15, 5]
+    plt.figure(num="Stock price chart")
+    plt.title("Stock price chart")
+    plt.xlabel("Date")
+    plt.ylabel(f'Price [{currency}]')
+    plt.plot(dates, prices, label="Price")
+    return
+
+
+def set_MACD_chart(dates, MACD, SIGNAL):
+    plt.figure(num="MACD and SIGNAL chart")
+    plt.title("MACD and SIGNAL chart")
+    plt.xlabel("Date")
+    plt.ylabel("MACD and SIGNAL values")
+    plt.plot(dates, MACD, label="MACD")
+    plt.plot(dates, SIGNAL, label="SIGNAL")
+    plt.legend(loc="lower right", fancybox=True, shadow=True, borderpad=1)
+    return
+
+
+def load_data(path, date_column, price_column):
     data = pd.read_csv(path, header=None)
     data = data.values.tolist()
     data.pop(0)  # removing unnecessary titles of columns
@@ -15,7 +36,7 @@ def load_data(path, price_column, date_column):
         prices.append(float(line[price_column]))  # here: 1-opening prices, 4-closing prices
         dates.append(datetime.datetime.strptime(line[date_column], "%Y-%m-%d"))
         rows_number += 1
-    return prices, dates, rows_number
+    return dates, prices, rows_number
 
 
 def EMA(samples, N, offset):
@@ -43,35 +64,37 @@ def create_MACD(prices, rows_number):
 def create_SIGNAL(MACD, rows_number):
     SIGNAL = []
     for i in range(rows_number):
-        if i < 26:
+        if i < 35:
             SIGNAL.append(0)
         else:
             SIGNAL.append(EMA(MACD, 9, i))
     return SIGNAL
 
 
-def MACD_simulation(prices, dates, MACD, SIGNAL, rows_number, starting_budget, starting_actions,
+def MACD_simulation(prices, dates, MACD, SIGNAL, rows_number, starting_budget, starting_actions, currency,
                     buying_multiplier=1.0, selling_multiplier=1.0, isDivisible=False):
-    #buying_multiplier = 1.0 -> all in, 0.5 -> spend only 50% of budget to buy actions, 0.25 -> 25%, etc.
-    #selling_multiplier -> as above but with selling
-    #isDivisible = False -> actions are not divisible, cryptocurrencies are
+    # buying_multiplier = 1.0 -> all in, 0.5 -> spend only 50% of budget to buy actions, 0.25 -> 25%, etc.
+    # selling_multiplier -> as above but with selling
+    # isDivisible = False -> actions are not divisible, cryptocurrencies are
     budget = starting_budget
     actions = starting_actions
-
     isOver = False  # is MACD over SIGNAL
+    isOnPlot = [False, False]
     plt.figure(1)
-    if MACD[26] > SIGNAL[26]:
+    if MACD[35] > SIGNAL[35]:
         isOver = True
-    for i in range(26, rows_number):
+    for i in range(35, rows_number):
         if isOver and MACD[i] < SIGNAL[i]:  # selling
             if isDivisible:
                 budget += round(selling_multiplier * actions * prices[i], 2)
                 actions -= selling_multiplier * actions
             else:
-                budget += round(round(selling_multiplier * actions) * prices[i], 2)
-                actions -= round(selling_multiplier*actions)
+                sold_actions = round(selling_multiplier * actions)
+                budget += round(sold_actions * prices[i], 2)
+                actions -= sold_actions
             isOver = False
-            plt.plot(dates[i], prices[i], "yo")
+            plt.plot(dates[i], prices[i], "go", label="sell" if not isOnPlot[0] else "")
+            isOnPlot[0] = True
         elif not isOver and MACD[i] > SIGNAL[i]:  # buying
             if isDivisible:
                 actions += buying_multiplier * budget / prices[i]
@@ -81,27 +104,27 @@ def MACD_simulation(prices, dates, MACD, SIGNAL, rows_number, starting_budget, s
                 actions += new_actions
                 budget -= round(new_actions * prices[i], 2)
             isOver = True
-            plt.plot(dates[i], prices[i], "ro")
+            plt.plot(dates[i], prices[i], "ro", label="buy" if not isOnPlot[1] else "")
+            isOnPlot[1] = True
+    plt.legend(loc="lower right", fancybox=True, shadow=True, borderpad=1)
     budget += round(actions * prices[rows_number-1], 2)
-    profit = round(budget - starting_budget, 2)
-    print("In the end we have " + str(round(budget, 2)) + "j which gives us " + str(profit) + "j (" +
-          str(round(profit/starting_budget * 100, 2)) + "%) of profit using MACD indicator.")
+    start = starting_budget + starting_actions * prices[0]
+    profit = round(budget - start, 2)
+    print("In the end we have " + str(round(budget, 2)) + " " + currency + " which gives us " + str(profit) + " " +
+          currency + " (" + str(round(profit/start * 100, 2)) + "%) of profit using MACD indicator.")
     return
 
 
 def main():
-    plt.rcParams['figure.figsize'] = [15, 5]
-    prices, dates, rows_number = load_data("data2.csv", 1, 0)
-    plt.plot(dates, prices)
+    dates, prices, rows_number = load_data("data.csv", 0, 1)
+    currency = "PLN"
+    set_price_chart(dates, prices, currency)
 
     MACD = create_MACD(prices, rows_number)
-    plt.figure(2)
-    plt.plot(dates, MACD)
-
     SIGNAL = create_SIGNAL(MACD, rows_number)
-    plt.plot(dates, SIGNAL)
+    set_MACD_chart(dates, MACD, SIGNAL)
 
-    MACD_simulation(prices, dates, MACD, SIGNAL, rows_number, 10000, 0)
+    MACD_simulation(prices, dates, MACD, SIGNAL, rows_number, 0, 1000, currency)
 
     plt.show()
     return
